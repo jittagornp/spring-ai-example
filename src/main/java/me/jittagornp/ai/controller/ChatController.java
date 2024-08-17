e9cdb5c2736ac446982623a668a877e993e00976
Jittagorn Pitakmetagoon <jittagorn.pitakmetagoon1@lotuss.com>
2024-08-17 14:07:30 +0700

    refactor: Use lombok @RequiredArgsConstructor instead of @Autowired

--- src/main/java/me/jittagornp/ai/controller/ChatController.java
+++ src/main/java/me/jittagornp/ai/controller/ChatController.java
@@ -1,10 +1,10 @@
 package me.jittagornp.ai.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +13,13 @@ import reactor.core.publisher.Flux;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
 
     private final OpenAiChatModel chatModel;
 
-    @Autowired
-    public ChatController(OpenAiChatModel chatModel) {
-        this.chatModel = chatModel;
-    }
-
     @GetMapping("/ai/generate")
     public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-
         return Map.of("generation", chatModel.call(message));
     }
 
--- src/main/java/me/jittagornp/ai/controller/EmbeddingController.java
+++ src/main/java/me/jittagornp/ai/controller/EmbeddingController.java
@@ -1,8 +1,8 @@
 package me.jittagornp.ai.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +11,14 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class EmbeddingController {
 
     private final EmbeddingModel embeddingModel;
 
-    @Autowired
-    public EmbeddingController(EmbeddingModel embeddingModel) {
-        this.embeddingModel = embeddingModel;
-    }
-
     @GetMapping("/ai/embedding")
     public Map embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        EmbeddingResponse embeddingResponse = this.embeddingModel.embedForResponse(List.of(message));
+        EmbeddingResponse embeddingResponse = embeddingModel.embedForResponse(List.of(message));
         return Map.of("embedding", embeddingResponse);
     }
 
